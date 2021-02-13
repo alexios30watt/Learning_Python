da64d0f246017f775fb8b7e81d99395086199bed
alexios30watt <70859852+alexios30watt@users.noreply.github.com>
2021-02-13 12:04:37 +0100

    vPyhton ergänzt
    Animation des Balls

--- Lernplattform/Mathematische_Uebungen/Plotter.py
+++ Lernplattform/Mathematische_Uebungen/Plotter.py
@@ -1,6 +1,12 @@
-import numpy as np
-import matplotlib.pyplot as plt
-x=np.arange(-20,6,0.01)
-y=x**3-7*x**2+7*x+15
-plt.plot(x,y)
-plt.show()
+#import numpy as np
+#import matplotlib.pyplot as plt
+
+#x=np.arange(1,6,0.01)
+#y=x-7*x**2+7*x+15
+#plt.plot(x,y)
+#plt.show()
+
+#import datetime
+#now = datetime.datetime.now()
+#print now('%d.%m.%Y')) 
+
