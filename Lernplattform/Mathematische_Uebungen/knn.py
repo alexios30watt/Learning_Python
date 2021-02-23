2874ea3ae2320dbd518094029554e54464df43be
alexios30watt <70859852+alexios30watt@users.noreply.github.com>
2021-02-23 21:05:41 +0100

    Update KNN
    Update KNN

--- Lernplattform/Mathematische_Uebungen/knn.py
+++ Lernplattform/Mathematische_Uebungen/knn.py
@@ -1,13 +1,16 @@
+from functools import cmp_to_key
 import numpy as np
 from sklearn import datasets
 from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
-from matplotlib.colors import listed
-cmap = ListedColormap (['#FF0000', '#00FF00', '#0000FF'])
-
+from matplotlib.colors import ListedColormap
+cmap = ListedColormap(['#FF0000', '#00FF00', '#0000FF'])
 iris = datasets.load_iris()
 X, y = iris.data, iris.target
-
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0,2, random_state=1234)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1234)
 
 print(X_train.shape)
+print(X_train[0])
+
+print(y_train.shape)
+print(y_train)
